818f8053662ffe61c28952f3c675df06f4f66766
WarLikeLaux <warlikelaux080399@mail.ru>
2026-02-15 15:25:49 +0600

    #1 - implemented ECS core with Entity type, World factory and movement system

---
+++ src/core/ecs.ts
@@ -0,0 +1,13 @@
+import { World } from 'miniplex';
+import { Object3D, Vector3 } from 'three';
+
+export type Entity = {
+	id: string;
+	position?: Vector3;
+	velocity?: Vector3;
+	view?: Object3D;
+};
+
+export function createWorld() {
+	return new World<Entity>();
+}
---
+++ src/core/systems.ts
@@ -0,0 +1,17 @@
+import { World } from 'miniplex';
+import { Entity } from '@/core/ecs';
+
+export function runMovementSystem(world: World<Entity>, dt: number) {
+	const query = world.with('position', 'velocity');
+
+	for (const entity of query) {
+		const { position, velocity } = entity;
+		const movement = velocity.clone().multiplyScalar(dt);
+
+		position.add(movement);
+
+		if (entity.view) {
+			entity.view.position.copy(position);
+		}
+	}
+}
