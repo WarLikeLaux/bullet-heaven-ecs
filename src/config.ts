7ff7aaf1b7ce11be61d7799209ba769eee7903c6
WarLikeLaux <warlikelaux080399@mail.ru>
2026-02-15 16:23:05 +0600

    #2 - added centralized game config with player constants

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const PLAYER_SPEED = 3;
+export const SPRITE_SCALE = 2;
+export const CAMERA_ZOOM = 10;
+export const ANIMATION_FPS = 8;
+export const PLAYER_SPRITE = 'assets/characters/pipo-nekonin008.png';
