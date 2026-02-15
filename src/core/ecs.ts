ba059d62cc9cc926a1280b5ba8ba0a144fce6545
WarLikeLaux <warlikelaux080399@mail.ru>
2026-02-15 16:22:34 +0600

    #2 - added ECS Entity type with SpriteAnimation and PlayerInput components

--- src/core/ecs.ts
+++ src/core/ecs.ts
@@ -1,11 +1,35 @@
 import { World } from 'miniplex';
 import { Object3D, Vector3 } from 'three';
 
+export const DIRECTION_DOWN = 0;
+export const DIRECTION_LEFT = 1;
+export const DIRECTION_RIGHT = 2;
+export const DIRECTION_UP = 3;
+
+export type SpriteAnimation = {
+  frameIndex: number;
+  frameCount: number;
+  frameDuration: number;
+  elapsed: number;
+  direction: number;
+  isMoving: boolean;
+};
+
+export type PlayerInput = {
+  up: boolean;
+  down: boolean;
+  left: boolean;
+  right: boolean;
+};
+
 export type Entity = {
   id: string;
   position?: Vector3;
   velocity?: Vector3;
+  speed?: number;
   view?: Object3D;
+  playerInput?: PlayerInput;
+  spriteAnimation?: SpriteAnimation;
 };
 
 export function createWorld() {
